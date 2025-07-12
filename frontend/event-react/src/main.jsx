d4c0032553bb46d50162f21949c2405a1be44223
Ritik Ranjan <ritikranjan999@gmail.com>
2025-07-12 16:18:51 +0530

    feat: update dependencies and add new components
    - Updated package.json to include new dependencies such as Radix UI components, Tailwind CSS, and others.
    - Removed unused CSS file import in App.jsx.
    - Created a new Navbar component.
    - Refactored index.css to integrate Tailwind CSS and custom styles.
    - Set up React Router in main.jsx with routes for AttendeeLandingPage and LoginPage.
    - Added new pages: AttendeeLandingPage, LoginPage, and Dashboard with basic structure.
    - Updated Vite configuration to include Tailwind CSS plugin.

--- frontend/event-react/src/main.jsx
+++ frontend/event-react/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
 import './index.css'
 import App from './App.jsx'
+import AttendeeLandingPage from './pages/AttendeeLandingPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: AttendeeLandingPage,
+  },
+  {
+    path: '/login',
+    Component: LoginPage
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontend/event-react/src/pages/AttendeeLandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AttendeeLandingPage = () => {
+    return (
+        <div>AttendeeLandingPage</div>
+    )
+}
+
+export default AttendeeLandingPage
